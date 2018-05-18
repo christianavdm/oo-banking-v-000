171b6a384303e94a3f1bccce0f594f351e3a26c9
Christiana Vandermale <christianavdm@gmail.com>
2018-05-18 01:12:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Transfer
   attr_reader :sender, :receiver, :status, :amount
   
-  @last_transfer = { @sender => nil, @receiver => nil, @amount => nil }
+  @@last_transfer = { @sender => nil, @receiver => nil, @amount => nil }
   
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -24,14 +24,18 @@ class Transfer
     if @status == "pending" && self.valid? && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @last_transfer[@sender] = @sender 
-      @last_transfer[@receivenddr 
-      @last_transfer[@sender] = @sender 
+      @@last_transfer[@sender] = @sender 
+      @@last_transfer[@receiver] = @receiver 
+      @@last_transfer[@amount] = @amount
       @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
+  end
+  
+  def reverse_transfer
+    
   end
   
 end
