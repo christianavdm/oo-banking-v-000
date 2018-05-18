148e18ab31976adee893da76282226b178a733a5
Christiana Vandermale <christianavdm@gmail.com>
2018-05-18 01:02:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      return "transaction incomplete"
+      return "Transaction rejected. Please check your account balance."
     end
   end
   
